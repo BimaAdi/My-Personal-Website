e9f27f186765f5fb7312e2ae1ef8b74007296ba6
BimaAdi <bimaadi419@gmail.com>
2023-05-17 00:01:53 +0700

    refactor to component

---
+++ src/app/components/ContactMe.tsx
@@ -0,0 +1,20 @@
+
+const ContactMe = () => {
+    return (
+        <section
+            style={{ height: "500px" }}
+            className="w-100 flex flex-col items-center justify-center"
+        >
+            <h2 className="text-center text-3xl pb-5">Contact Me</h2>
+            <div className="h-0.5 w-3/12 border-b-2 bg-secondary"></div>
+            <p className="mt-5 w-11/12 md:w-9/12 lg:w-7/12 text-justify text-lg">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+                velit adipisci quae? Magni aut quis nostrum quibusdam iste quidem
+                dolores, natus laborum odit vero, aliquam neque ipsa nihil nisi
+                pariatur.
+            </p>
+        </section>
+    )
+}
+
+export default ContactMe;
---
+++ src/app/components/WorkingExperience.tsx
@@ -0,0 +1,19 @@
+const WorkingExperience = () => {
+    return (
+        <section
+            style={{ height: "500px" }}
+            className="w-100 flex flex-col items-center justify-center"
+        >
+            <h2 className="text-center text-3xl pb-5">Working Experience</h2>
+            <div className="h-0.5 w-3/12 border-b-2 bg-secondary"></div>
+            <p className="mt-5 w-11/12 md:w-9/12 lg:w-7/12 text-justify text-lg">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
+                cupiditate animi vitae! Atque recusandae deserunt expedita corrupti
+                numquam eum esse laborum qui, odit ad delectus necessitatibus nobis.
+                Accusamus, eveniet distinctio?
+            </p>
+        </section>
+    )
+}
+
+export default WorkingExperience;
