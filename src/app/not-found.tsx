d94fb674f39a4ae3d6b9fddcac16467e6df6126a
BimaAdi <bimaadi419@gmail.com>
2023-05-23 22:49:26 +0700

    modified not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import NotFound from "@/components/NotFound"
+import Full from "@/layouts/Full"
+
+export default function Custom404() {
+    return (
+        <Full>
+            <NotFound />
+        </Full>
+    )
+}
---
+++ src/components/NotFound.tsx
@@ -0,0 +1,22 @@
+import { AiOutlineStop } from "react-icons/ai"
+
+const NotFound = () => {
+    return (
+        <div className="flex text-secondary items-center" style={{ height: "60px" }}>
+            <AiOutlineStop style={{
+                display: "block",
+                margin: "auto",
+                width: "60px",
+                height: "60px",
+            }} />
+            <div className="w-2 bg-white h-full mx-5"></div>
+            <div>
+                <h3 className="text-2xl md:text-5xl">Page Not Found</h3>
+                {/* work around link not working */}
+                <a href="/">Go To Home Page</a>
+            </div>
+        </div>
+    )
+}
+
+export default NotFound
