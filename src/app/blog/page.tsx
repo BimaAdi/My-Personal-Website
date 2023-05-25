4160a37b27e65bfb9d9aed84aea4cbf50a3db89e
BimaAdi <bimaadi419@gmail.com>
2023-05-26 06:59:25 +0700

    add simple blog item

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,34 @@
-import OnProgress from "@/components/OnProgress"
+import Link from "next/link";
 import Main from "@/layouts/Main"
+import BlogItem from "@/components/BlogItem";
 
 export default function Blog() {
     return (
         <Main>
-            <OnProgress />
+            <div style={{ height: "70px" }}></div> {/* navbar buffer */}
+            <main className="w-full md:max-w-3xl md:mx-auto text-white">
+                <div className="m-4">
+                    <input placeholder="search"/>
+                </div>
+                <BlogItem 
+                    title="How to structure your Rust Project" 
+                    link="/" 
+                    tags={["#Rust", "#Cargo"]}
+                    created_at={new Date("2023-03-27")}
+                />
+                <BlogItem 
+                    title="Scaling Websocket in Go" 
+                    link="/" 
+                    tags={["#Websocket", "#Go", "#Redis"]}
+                    created_at={new Date("2023-03-15")}
+                />
+                <BlogItem 
+                    title="Programing in Canvas using reactjs" 
+                    link="/" 
+                    tags={["#Javascript", "#React"]} 
+                    created_at={new Date("2023-02-02")}
+                />
+            </main>
         </Main>
     )
 }
---
+++ src/components/BlogItem.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+type Props = {
+    title: string
+    link: string
+    tags: string[]
+    created_at: Date
+}
+
+const monthNames = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+
+const BlogItem = ({ title, link, tags, created_at }: Props) => {
+    return (
+        <div className="m-4 rounded-md bg-gray">
+            <h2 className="px-8 pt-2 pb-1 text-2xl font-bold hover:underline hover:cursor-pointer">
+                <Link href={link}>{title}</Link>
+            </h2>
+            <div className="flex w-full justify-between items-center pt-1 pb-2">
+                <p className="px-8 text-xs">
+                    {tags.join(" ")}
+                </p>
+                <p className="px-8 text-xs">
+                    {created_at.getDay()} {monthNames[created_at.getMonth()]} {created_at.getFullYear()}
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default BlogItem;
