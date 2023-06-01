a1f0acdfb9db5b7186c07893ca6290ba15af4ddd
BimaAdi <bimaadi419@gmail.com>
2023-06-01 08:17:29 +0700

    refactor queryclient

--- src/client/components/blog/Blog.tsx
+++ src/client/components/blog/Blog.tsx
@@ -10,52 +10,36 @@ import customParseFormat from "dayjs/plugin/customParseFormat";
 dayjs.extend(customParseFormat)
 
 const Blog = () => {
-    const [blogsState, setBlogState] = useState<{
-        id: string,
-        title: string,
-        link: string,
-        tags: string[],
-        created_at: Date
-    }[]>([])
+    const [paginateBlogParams, setPaginateBlogParams] = useState<{ 
+        page: number, 
+        page_size: number 
+    }>({ page: 1, page_size: 10 });
 
     // Query
-    const { status: qpbStatus, data: qpbData, isRefetching: qpbIsRefetching, refetch: qpbRefetch } = useQuery({
-        queryKey: ['blogs'],
-        queryFn: () => GetPaginateBlog({ page: 1, page_size: 10 }),
+    const paginateBlogQuery = useQuery({
+        queryKey: ['blogs', paginateBlogParams],
+        queryFn: () => GetPaginateBlog(paginateBlogParams),
     })
-    useEffect(() => {
-        if (qpbStatus === "success") {
-            if (qpbData.status === 200) {
-                setBlogState(qpbData.json.results.map((x) => {
-                    return {
-                        id: x.id,
-                        title: x.title,
-                        link: x.link,
-                        tags: x.tags.map(y => y?.name || ""),
-                        created_at: dayjs(x.created_at, "YYYY-MM-DD").toDate()
-                    }
-                }))
-            }
-        }
-    }, [qpbStatus, qpbIsRefetching, qpbData])
 
     // Action
     const searchClick = () => {
-        qpbRefetch()
+        paginateBlogQuery.refetch()
     }
 
     return (
         <main className="w-full md:max-w-3xl md:mx-auto text-white">
             <BlogSearchInput searchFn={searchClick} />
-            {blogsState.map((x) => {
+            {paginateBlogQuery.status === "loading" ? <div className="m-4">Loading...</div> : <></>}
+            {paginateBlogQuery.data?.status === 200 ? paginateBlogQuery.data.json?.results.map((x) => {
                 return <BlogItem
                     key={x.id}
                     title={x.title}
                     link={x.link}
-                    tags={x.tags}
-                    created_at={x.created_at}
+                    tags={x.tags.map(y => y?.name || "")}
+                    created_at={dayjs(x.created_at, "YYYY-MM-DD").toDate()}
                 />
-            })}
+            }) : <></>}
+            {paginateBlogQuery.data?.status === 500 ? <div>Something Wrong with seerver</div> : <></>}
         </main>
     )
 }
