321b3b6ace0d14592b4af5c3077fdd359196fccf
BimaAdi <bimaadi419@gmail.com>
2023-06-13 22:01:55 +0700

    update project page

--- e2e/project.spec.ts
+++ e2e/project.spec.ts
@@ -4,6 +4,6 @@ test("/project page", async ({ page }) => {
   await page.goto("http://localhost:3000/project");
   await expect(page).toHaveTitle(/BimaAdi/);
 
-  const onProgress = page.locator("#onProgress");
-  await expect(onProgress).toContainText("On Progress");
+  // const onProgress = page.locator("#onProgress");
+  // await expect(onProgress).toContainText("On Progress");
 });
--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,10 +1,41 @@
-import OnProgress from "@/client/components/OnProgress"
+import Image from "next/image";
+import { AiOutlineLink, AiFillGithub } from "react-icons/ai";
 import Main from "@/client/layouts/Main"
 
-export default function Blog() {
+export default function Project() {
     return (
-        <Main addNavbarBuffer={false} addFooterBuffer={false}>
-            <OnProgress />
+        <Main>
+            <div className="w-100 text-white p-2">
+                <div className="max-w-md bg-gray border-solid border-2 border-gray rounded-lg">
+                    <Image
+                        src="/My Personal Website.png"
+                        alt="bima"
+                        className="w-full rounded-lg"
+                        width={1000}
+                        height={600}
+
+                    />
+                    <div className="p-2">
+                        <div className="text-justify">This personal website</div>
+                        <div className="w-100 flex justify-end align-middle">
+                            <AiOutlineLink
+                                style={{
+                                    display: "block",
+                                    width: "30px",
+                                    height: "30px",
+                                }}
+                            />
+                            <AiFillGithub
+                                style={{
+                                    display: "block",
+                                    width: "30px",
+                                    height: "30px",
+                                }}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Main>
     )
 }
